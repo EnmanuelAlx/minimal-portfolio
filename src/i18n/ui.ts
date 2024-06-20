dad402420f5710fdbb5420eb9a282c067fab4893
INGCapaDev <ingcapadev@gmail.com>
2024-06-20 13:38:39 -0700

    :sparkles: [FEAT](lang): Add i18n internalization and some ui translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,41 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    title: 'Resume of',
+    'section.about': 'About',
+    'section.experience': 'Experience',
+    'section.education': 'Education',
+    'section.projects': 'Projects',
+    'section.certifications': 'Certifications',
+    'section.skills': 'Skills',
+    'section.softSkills': 'Soft Skills',
+    'hero.sendEmail': 'Send an email to',
+    'hero.emailFollowing': 'at the following address:',
+    'hero.phone': 'Call',
+    'hero.phoneFollowing': 'at the following number:',
+    'hero.social': 'Visit',
+    'hero.socialFollowing': 'profile at the following link:',
+  },
+  es: {
+    title: 'Currículum de',
+    'section.about': 'Sobre mí',
+    'section.experience': 'Experiencia',
+    'section.education': 'Educación',
+    'section.projects': 'Proyectos',
+    'section.certifications': 'Certificaciones',
+    'section.skills': 'Tecnologías',
+    'section.softSkills': 'Habilidades blandas',
+    'hero.sendEmail': 'Envía un correo a',
+    'hero.emailFollowing': 'a la siguiente dirección:',
+    'hero.phone': 'Llama a',
+    'hero.phoneFollowing': 'al siguiente número:',
+    'hero.social': 'Visita el perfil de',
+    'hero.socialFollowing': 'en el siguiente enlace:',
+  },
+} as const;
