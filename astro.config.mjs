111a8cfd531bf28f1dca8ee7843ed124137156c7
INGCapaDev <ingcapadev@gmail.com>
2024-06-24 13:41:37 -0700

    :zap: [CHORE]: Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
   },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'es'],
+      },
+    }),
+    robotsTxt(),
+  ],
 });
